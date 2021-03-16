af7c19df19524743ce607e55e4e1c504d9f28502
nishan2003 <43458723+nishan2003@users.noreply.github.com>
2021-03-15 22:35:22 -0600

    changes

--- src/main/java/frontend/App.java
+++ src/main/java/frontend/App.java
@@ -4,42 +4,27 @@ package frontend;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import stockdata.*;
-
 import javax.swing.*;
-
-
+import java.awt.*;
 
 
 public class App {
     public static void main(String[] args) {
 
-        Stock MSFT = new Stock("MSFT");
+        Stock stock = new Stock("RY");
 
-        final JFreeChart chart = CreateTimeSeries.create(MSFT);
+        final JFreeChart chart = CreateTimeSeries.create(stock);
+        chart.setBackgroundPaint(new Color(25,28,38));
         final ChartPanel chartPanel = new ChartPanel( chart );
+        chartPanel.setBackground(new Color(25,28,38));
         chartPanel.setPreferredSize( new java.awt.Dimension( 200 , 200 ) );
         chartPanel.setMouseZoomable( true , false );
-        JFrame hi = new JFrame();
-        hi.setSize(400, 400);
-        hi.add(chartPanel);
-        hi.setVisible(true);
-        hi.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        JFrame main_panel = new JFrame();
+        main_panel.setSize(650, 440);
+        main_panel.add(chartPanel);
+        main_panel.setTitle("Stock Viewer");
+        main_panel.setVisible(true);
+        main_panel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
 }
--- src/main/java/frontend/CreateTimeSeries.java
+++ src/main/java/frontend/CreateTimeSeries.java
@@ -3,6 +3,7 @@ package frontend;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
+import org.jfree.data.general.SeriesException;
 import org.jfree.data.time.Day;
 import org.jfree.data.time.TimeSeries;
 import org.jfree.data.time.TimeSeriesCollection;
@@ -21,12 +22,16 @@ public class CreateTimeSeries{
 
         Day current = new Day();
         for(int i= 0; i < adjclose.size(); i++) {
+            try {
+                series.add(new Day(dates.get(i)), adjclose.get(i));
+            }catch (SeriesException ignored) {
 
-            series.add(new Day(dates.get(i)), adjclose.get(i));
+            }
+
         }
 
         XYLineAndShapeRenderer renderer = new XYLineAndShapeRenderer();
-        renderer.setSeriesPaint(0, new Color(142,123,211));
+        renderer.setSeriesPaint(0, new Color(141,17,71));
         renderer.setSeriesShapesVisible(0, false);
         renderer.setDefaultStroke(new BasicStroke(2.5f));
         renderer.setAutoPopulateSeriesStroke(false);
@@ -34,18 +39,19 @@ public class CreateTimeSeries{
         TimeSeriesCollection dataset = new TimeSeriesCollection(series);
 
         JFreeChart chart = ChartFactory.createTimeSeriesChart(
-                stock.getSymbol(),
+                "",
                 "",
                 "",
                 dataset,
                 false,
                 false,
                 false);
-        chart.getPlot().setBackgroundPaint(new Color(12,12,12));
+        chart.getPlot().setBackgroundPaint(new Color(25,28,38));
         chart.getXYPlot().setRangeGridlinesVisible(false);
         chart.getXYPlot().setDomainGridlinesVisible(false);
         chart.getXYPlot().getRangeAxis().setVisible(false);
         chart.getXYPlot().getDomainAxis().setVisible(false);
+        chart.getXYPlot().setOutlinePaint(null);
         chart.getXYPlot().setRenderer(renderer);
 
         return chart;
